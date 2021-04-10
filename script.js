379661b5dff98c7d709b9c41514e91cd18e4ed07
JustTom77 <20codetom20@gmail.com>
2021-04-11 01:35:10 +0200

    add twitter share button

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// Get Jokes From Api
-
+// function to reduce the Fontsize when Jokes over 85 chars
 function addLongJokeClass() {
   if (chuckJokes.innerHTML.length > 85) {
     chuckJokes.classList.add("long-joke");
@@ -9,29 +8,33 @@ function addLongJokeClass() {
 }
 
 let apiJokes = [];
+// Get Jokes From Api
 async function getJokes() {
   const apiUrl = "https://api.chucknorris.io/jokes/random";
   try {
     const response = await fetch(apiUrl);
     apiJokes = await response.json();
     document.getElementById("joke").innerHTML = apiJokes.value;
-   addLongJokeClass()
-  
-    console.log(chuckJokes.innerHTML.length);
-    console.log(apiJokes.value);
+    addLongJokeClass();
   } catch (error) {
     alert(error);
   }
 }
-//On Load
+// On Load
 getJokes();
 
 // Variables
 const chuckJokes = document.getElementById("joke");
 const button = document.getElementById("new-joke");
 const tweetButton = document.getElementById("twitter");
-
+// on Button click fill in the Joke
 button.addEventListener("click", () => {
   getJokes();
   chuckJokes.innerHTML = apiJokes.value;
+});
+// on tweetButton click share joke on Twitter
+tweetButton.addEventListener("click", () => {
+  window.open(
+    "http://www.twitter.com/intent/tweet?text=" + chuckJokes.innerHTML + "'"
+  );
 });
